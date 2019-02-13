1f7af0997fd8bb1fea23796a7130d7d0674d3768
Ren <nsoharab@gmail.com>
2019-02-13 18:40:29 +0200

    Response test

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -14,6 +14,7 @@ class ImageController extends Controller
   {
 
     $newImage = $request->query('url');
+    return response()->json(['mediaThumbnail' => $newImage]);
     $newWidth = $request->query('w');
     $newHeight = $request->query('h');
     $exif = $request->query('exif');
